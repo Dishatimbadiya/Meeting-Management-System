9bbe120a1098c9597552405102c9cf3198b08988
yash timbadiya <dishatimbadiya055@gmail.com>
2024-10-14 17:14:44 +0530

    code added

---
+++ Events.Web/Models/EventInputModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using Events.Data;
+
+namespace Events.Web.Models
+{
+    public class EventInputModel
+    {
+        [Required(ErrorMessage = "Event title is required.")]
+        [StringLength(200, ErrorMessage = "The {0} must be between {2} and {1} characters long.",
+            MinimumLength = 1)]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date and Time")]
+        [FutureDate(ErrorMessage = "The meeting date must be in the future.")]
+        public DateTime StartDateTime { get; set; }
+
+        public TimeSpan? Duration { get; set; }
+
+        public string Description { get; set; }
+
+        [MaxLength(200)]
+        public string Location { get; set; }
+
+        [Display(Name = "Is Public?")]
+        public bool IsPublic { get; set; }
+
+        public static EventInputModel CreateFromEvent(Event e)
+        {
+            return new EventInputModel()
+            {
+                Title = e.Title,
+                StartDateTime = e.StartDateTime,
+                Duration = e.Duration,
+                Location = e.Location,
+                Description = e.Description,
+                IsPublic = e.IsPublic,
+            };
+        }
+    }
+
+    // Custom validation attribute for future dates
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime dateTime)
+            {
+                if (dateTime <= DateTime.Now)
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
